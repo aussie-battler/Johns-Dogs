b50a900ac56ca22bd69a701eb8b12120e0a8de34
gianni001 <john_oli@hotmail.com>
2016-02-25 22:22:15 +1000

    sounds

---
+++ sounds/defines.hpp
@@ -0,0 +1,34 @@
+class CfgSounds
+{
+    sounds[] = {bl_begin,bl_hit1,bl_hit2,bl_hit3,bl_wave1,bl_wave2,bl_wave3,bl_psi,bl_full,bl_detect,apsi_on,apsi_off,ns_fx_misc4,ns_fx_drone1,ns_fx_drone2};
+    class dog_growl
+    {
+        name = "dog_growl";
+        sound[] = {"sounds\dog_growl.ogg",1.0,1.0};
+        titles[] = {};
+    };
+    class dog_one
+    {
+        name = "dog_one";
+        sound[] = {"sounds\dog_one.ogg",1.0,1.0};
+        titles[] = {};
+    };
+    class dog_ruff
+    {
+        name = "dog_ruff";
+        sound[] = {"sounds\dog_ruff.ogg",1.0,1.0};
+        titles[] = {};
+    };
+    class dog_whine
+    {
+        name = "dog_whine";
+        sound[] = {"sounds\dog_whine.ogg",1.0,1.0};
+        titles[] = {};
+    };
+    class whistle
+    {
+        name = "whistle";
+        sound[] = {"sounds\whistle.ogg",1.0,1.0};
+        titles[] = {};
+    };
+};
